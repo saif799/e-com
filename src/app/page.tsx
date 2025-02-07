677774f4aa9278b1cbb101f6c792de47101b7efd
saif799 <faysalbakziz@gmail.com>
2025-02-07 20:22:53 +0100

    fixed build error by adding a suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,19 @@ import Listings from "@/components/Listings";
 import { MainCarousel } from "@/components/mainCarousel";
 import { Button } from "@/components/ui/button";
 import { db } from "@/server/db";
-import { type products, type productSizes, shoeModels } from "@/server/db/schema";
+import {
+  type products,
+  type productSizes,
+  shoeModels,
+} from "@/server/db/schema";
 import type { InferSelectModel } from "drizzle-orm";
+import { Suspense } from "react";
 
 export type formattedProductsType = {
   products: InferSelectModel<typeof products>;
   shoe_models: InferSelectModel<typeof shoeModels>;
   product_sizes: InferSelectModel<typeof productSizes>[] | null;
-}
+};
 export default async function HomePage() {
   const [products, models] = await Promise.all([
     GetShowCaseProducts(),
@@ -22,13 +27,16 @@ export default async function HomePage() {
     console.log(error);
   }
   let iterator = { index: 0, id: "" };
-  const formattedProducts : formattedProductsType[] = [];
+  const formattedProducts: formattedProductsType[] = [];
   products.forEach((p, index) => {
     if (p.products.id !== iterator.id) {
       iterator = { index: index, id: p.products.id };
-      formattedProducts.push({products : p.products,shoe_models: p.shoe_models, product_sizes: [p.product_sizes]});
-    }
-    else if(p.products.id === iterator.id) {
+      formattedProducts.push({
+        products: p.products,
+        shoe_models: p.shoe_models,
+        product_sizes: [p.product_sizes],
+      });
+    } else if (p.products.id === iterator.id) {
       formattedProducts[iterator.index]?.product_sizes?.push(p.product_sizes);
     }
   });
@@ -36,9 +44,11 @@ export default async function HomePage() {
   return (
     <main>
       <div className="flex flex-col items-center justify-center gap-8 pb-8">
-        <MainCarousel models={models.filter((p)=>{
-          return (p.desktopImage) && (p.mobileImage);
-        })} />
+        <MainCarousel
+          models={models.filter((p) => {
+            return p.desktopImage && p.mobileImage;
+          })}
+        />
 
         <Button
           variant={"ghost"}
@@ -46,7 +56,9 @@ export default async function HomePage() {
         >
           View Product
         </Button>
-        <Listings products={formattedProducts} models={models} />
+        <Suspense>
+          <Listings products={formattedProducts} models={models} />
+        </Suspense>
       </div>
     </main>
   );
